de59e68b2f67f70c3bc25c20e9917b4b4281faaa
Sarthak-Kakkar-03 <104781539+Sarthak-Kakkar-03@users.noreply.github.com>
2024-09-16 10:08:00 -0400

    node py made

--- agent_graph/node.py
+++ agent_graph/node.py
@@ -1,2 +1,9 @@
 from info_agent import info_chain
 from supervisor import supervisor_chain
+from mail_agent import mail_chain, tool_node
+from message import message_chain
+from configs import set_env
+
+
+set_env()
+
